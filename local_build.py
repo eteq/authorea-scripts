e3da8b1ec2093717e8273e87864bb1b4a8c18ba4
Erik Tollerud <erik.tollerud@gmail.com>
2015-12-22 18:22:04 -0500

    renames to make it slightly clearer whats going on

--- local_build.py
+++ local_build.py
@@ -66,15 +66,15 @@ def get_input_string(filename, localdir, quotepath=True):
     return r'\input{' + quote_chr + os.path.join(localdir, filename) + quote_chr + '}'
 
 
-def get_figure_string(filename, absdir, localdir):
+def get_figure_string(filename, localdir, inputdir):
     import json
 
     figdir, figfn = os.path.split(filename)
 
     figfnbase = os.path.splitext(figfn)[0]
-    figfn = os.path.join(localdir, figdir, figfn)
-    pdffn = os.path.join(absdir, figdir, figfnbase + '.pdf')
-    epsfn = os.path.join(absdir, figdir, figfnbase + '.eps')
+    figfn = os.path.join(inputdir, figdir, figfn)
+    pdffn = os.path.join(localdir, figdir, figfnbase + '.pdf')
+    epsfn = os.path.join(localdir, figdir, figfnbase + '.eps')
 
     if not os.path.exists(pdffn):
         pdffn = None
@@ -82,16 +82,16 @@ def get_figure_string(filename, absdir, localdir):
         epsfn = None
 
     if pdffn or epsfn:
-        figfn = os.path.join(localdir, figdir, figfnbase)
+        figfn = os.path.join(inputdir, figdir, figfnbase)
 
 
-    if os.path.exists(os.path.join(absdir, figdir, 'caption.tex')):
-        capfn = os.path.join(localdir, figdir, 'caption.tex')
+    if os.path.exists(os.path.join(localdir, figdir, 'caption.tex')):
+        capfn = os.path.join(inputdir, figdir, 'caption.tex')
         caption = r'\caption{ \protect\input{' + capfn + '}}'
     else:
         caption = ''
 
-    optsfn = os.path.join(absdir, figdir, 'latexfigopts.json')
+    optsfn = os.path.join(localdir, figdir, 'latexfigopts.json')
     figopts = FIGURE_DEFAULTS.copy()
     if os.path.exists(optsfn):
         with open(optsfn) as f:
